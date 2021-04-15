47b786e6c4e0c4be5724b5ab822b56b7418bdf65
Buckley212 <rob36611@gmail.com>
2021-04-15 09:52:36 -0400

    files added

--- scripts/hellLevel.js
+++ scripts/hellLevel.js
@@ -3,7 +3,7 @@ const canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 
 const backgroundImage = new Image();
-backgroundImage.src = './resources/assets/Hell_Map.png'
+backgroundImage.src = './resources/assets/hell_Map.png'
 
 const player_image = new Image();
 player_image.src = './resources/assets/sprites/soldier_idle_spritesheet.png'
